2b341a9796a173e2f8d1cd3648980a47fd1dd03a
ivan <kamillafortuna2005@gmail.com>
2026-08-24 21:36:36 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from yt_dlp import YoutubeDL 
+TOKEN ="8867316822:AAEyGwWssI4ZbxQwRThcspJbdvpXBhq6G1U"
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message) :
+     await message.answer("ghbdtn jnghfdm vyt ccskre yf nhtr cfeylrkfel b z crfxf. tuj")
+@dp.message()
+async def download_soundcloud(message: types.Message):
+        url = message.text
+        if "soundcloud.com" not in url:
+            await message.answer("лсщшуцйлфвысщйФ]") 
+            return
+        status_message = await message.answer("akz улю")
+        ydl_opts = {'format': 'bestaudio/best',
+                    'outtmlp': 'track.%(ext)s',
+                    'postprocessors':[{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec' : 'mp3',
+                        'preferredquality' : '192',
+
+                    }],
+                    'quiet' : True
+                     }
+        try:
+            loop = asyncio.get_event_loop()
+            with YoutubeDL(ydl_opts) as ydl:
+                await loop.run_in_executor(None, lambda:ydl.download([url]))
+                file_path = "track.mp3"
+                if os.path.exists(file_path):
+                    await status_message.edit_text("nhtr crfxfy")
+                    audio_file = types.FSInputFile(file_path)
+                    await message.answer_audio(audio=audio_file)
+                    os.remove(file_path)
+                    await status_message.delete()
+                else:
+                    await status_message.edit_text("❌ Ошибка: не удалось сохранить аудиофайл.")
+        except Exception as e:
+            print(f"Ошибка при скачивании: {e}")
+            await status_message.edit_text("❌ Произошла ошибка при обработке ссылки. Возможно, трек скрыт или заблокирован.")
+async def main():
+     print("Бот успешно запущен и готов качать музыку...")
+     await dp.start_polling(bot)
+if __name__ == "__main__":
+    asyncio.run(main())
